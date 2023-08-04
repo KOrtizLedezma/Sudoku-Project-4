cd45df475603fae2ffa624fb95baa96ee90513e7
Kwailan Rosell <krosell246@gmail.com>
2023-08-04 16:13:24 -0400

    Kwailan Rosell

--- board.py
+++ board.py
@@ -11,27 +11,24 @@ class Board:
         self.screen = screen
         self.difficulty = difficulty
         self.current_selected = None
-        self.board = [[Cell(0, row, col, self.screen) for col in range(self.width)] for row in range(self.height)]
+
 
 
     # Draws an outline of the Sudoku grid, with bold lines to delineate the 3x3 boxes.
     # Draws every cell on this board.
+
     def draw(self):
-        cell_size = 100
-        for row in range(self.height):
-            for col in range(self.width):
-                pygame.draw.rect(self.screen, (0, 0, 0),
-                                 ((col * cell_size), (row * cell_size), cell_size, cell_size), 1)
-
-                if (row % 3 == 0 or col % 3 == 0) and row > 0 and col > 0:
-                    pygame.draw.rect(self.screen, (0, 0, 0),
-                                     ((col * cell_size) - 2, (row * cell_size) - 2, cell_size + 4, cell_size + 4), 3)
-
-        # Draw each cell
-        for row in range(self.height):
-            for col in range(self.width):
-                cell = self.board[row][col]
+        for row in range(9):
+            for col in range(9):
+                cell = Cell(None, row+1, col+1, self.screen, None)
                 cell.draw()
+
+        pygame.draw.rect(self.screen, (0, 0, 0),
+                         ((4 * 80 -22), (1 * 80 -5), 6, 675))
+
+        pygame.draw.rect(self.screen, (0, 0, 0),
+                         ((7 * 80 - 38), (1 * 80 - 5), 6, 675))
+
 
     # Marks the cell at (row, col) in the board as the current selected cell.
     # Once a cell has been selected, the user can edit its value or sketched value.
@@ -55,9 +52,7 @@ class Board:
     # Sets the sketched value of the current selected cell equal to user entered value.
     # It will be displayed at the top left corner of the cell using the draw() function.
     def sketch(self, value):
-        row, col = self.current_selected
-        cell = self.board[row][col]
-        cell.set_sketched_value(value)
+        pass
 
     # Sets the value of the current selected cell equal to user entered value.
     # Called when the user presses the Enter key.
