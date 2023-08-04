b75bf678ca011bfeb64c2c9919766e229cad38a8
Kwailan Rosell <krosell246@gmail.com>
2023-08-04 14:13:46 -0400

    Kwailan

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import pygame
+from cell import Cell
 class Board:
 
     # Constructor for the Board class.
@@ -8,21 +10,42 @@ class Board:
         self.height = height
         self.screen = screen
         self.difficulty = difficulty
+        self.current_selected = None
+        self.board = [[Cell(0, row, col, self.screen) for col in range(self.width)] for row in range(self.height)]
+
 
     # Draws an outline of the Sudoku grid, with bold lines to delineate the 3x3 boxes.
     # Draws every cell on this board.
     def draw(self):
-        pass
+        cell_size = 100
+        for row in range(self.height):
+            for col in range(self.width):
+                pygame.draw.rect(self.screen, (0, 0, 0),
+                                 ((col * cell_size), (row * cell_size), cell_size, cell_size), 1)
+
+                if (row % 3 == 0 or col % 3 == 0) and row > 0 and col > 0:
+                    pygame.draw.rect(self.screen, (0, 0, 0),
+                                     ((col * cell_size) - 2, (row * cell_size) - 2, cell_size + 4, cell_size + 4), 3)
+
+        # Draw each cell
+        for row in range(self.height):
+            for col in range(self.width):
+                cell = self.board[row][col]
+                cell.draw()
 
     # Marks the cell at (row, col) in the board as the current selected cell.
     # Once a cell has been selected, the user can edit its value or sketched value.
     def select(self, row, col):
-        pass
+        self.current_selected = (row, col)
 
     # If a tuple of (x, y) coordinates is within the displayed board, this function returns a tuple of the (row, col)
     # of the cell which was clicked. Otherwise, this function returns None.
     def click(self, x, y):
-        pass
+        cell_size = 100
+        row, col = y // cell_size, x // cell_size
+        if row < self.height and col < self.width:
+            return row, col
+        return None
 
     # Clears the value cell. Note that the user can only remove the cell values and sketched value that are
     # filled by themselves.
@@ -32,7 +55,9 @@ class Board:
     # Sets the sketched value of the current selected cell equal to user entered value.
     # It will be displayed at the top left corner of the cell using the draw() function.
     def sketch(self, value):
-        pass
+        row, col = self.current_selected
+        cell = self.board[row][col]
+        cell.set_sketched_value(value)
 
     # Sets the value of the current selected cell equal to user entered value.
     # Called when the user presses the Enter key.
